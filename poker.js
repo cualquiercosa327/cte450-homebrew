9cc8eba597f14d8b1a1fc0905af9644bd0e3fade
Micah Elizabeth Scott <micah@misc.name>
2016-09-13 15:58:43 -0700

    Fix

--- poker.js
+++ poker.js
@@ -7,27 +7,25 @@ const dev = new HID.HID(0x56a, 0x17)
 var error_limit = 10;
 
 rl.on('line', (input) => {
-	const tokens = input.split(' ');
-	const addr = parseInt(tokens[0], 16);
-	const data = [0x11];
-	for (var i = 1; i <= 16; i++) {
-		data[i] = i < tokens.length ? parseInt(tokens[i], 16) : 0;
-	}
-	dev.sendFeatureReport([0x10, addr >> 8, addr & 0xff]);
-	dev.sendFeatureReport(data);
+    const tokens = input.split(' ');
+    const addr = parseInt(tokens[0], 16);
+    const data = [0x11];
+    for (var i = 1; i <= 16; i++) {
+        data[i] = i < tokens.length ? parseInt(tokens[i], 16) : 0;
+    }
+    dev.sendFeatureReport([0x10, addr >> 8, addr & 0xff]);
+    dev.sendFeatureReport(data);
 });
 
 dev.on('data', (data) => {
-	var adcr = data.readUInt16LE(3) >> 4;
-	var ticks = Math.round(Math.max(0, Math.min(1000, adcr - 160)));
-	console.log(data.toString('hex'), adcr, "#".repeat(ticks));
+    console.log(data.toString('hex'));
 });
 
 dev.on('error', (e) => {
-	console.log('err: ' + e);
-	if (error_limit-- > 0) {
-		setTimeout(() => {
-			dev.resume();
-		}, 10);
-	}
+    console.log('err: ' + e);
+    if (error_limit-- > 0) {
+        setTimeout(() => {
+            dev.resume();
+        }, 10);
+    }
 });
