7a33a439443dae76528cefeb2872d5aed6d2724b
Micah Elizabeth Scott <micah@misc.name>
2016-09-15 20:14:58 -0700

    Split the capture and filtering

--- detector.js
+++ detector.js
@@ -1,51 +1,32 @@
 #!/usr/bin/env node
 
-const HID = require('node-hid');
 const readline = require('readline');
-const fs = require('fs');
 const Fili = require('fili');
 const CircularBuffer = require('circular-buffer');
 
-const rl = readline.createInterface({input: process.stdin});
-const dev = new HID.HID(0x56a, 0x17)
-
-rl.on('line', (input) => {
-    // Write device memory
-
-    const tokens = input.split(' ');
-    const addr = parseInt(tokens[0], 16);
-    const data = [0x11];
-    for (var i = 1; i <= 16; i++) {
-        data[i] = i < tokens.length ? parseInt(tokens[i], 16) : 0;
-    }
-    // Set write address
-    dev.sendFeatureReport([0x10, addr >> 8, addr & 0xff]);
-    // Write exactly 16 bytes
-    dev.sendFeatureReport(data);
-});
-
 const firCalculator = new Fili.FirCoeffs();
 const firBandpass = new Fili.FirFilter(firCalculator.bandpass({
-    order: 32,
+    order: 256,
     Fs: 922,
-    F1: 10,
-    F2: 190,
+    F1: 4,
+    F2: 48,
 }));
 
-const history = new CircularBuffer(512);
+const history = new CircularBuffer(4096);
 
-function do_sample(adcr) {
-    // history.enq(firBandpass.singleStep(adcr));
+const rl = readline.createInterface({input: process.stdin});
+rl.on('line', (input) => {
+    const y = parseInt(input);
 
-    console.log(adcr);
+    history.enq(firBandpass.singleStep(y));
 
-    // var decoded = null;
-    // for (var bit_period = 0.9; bit_period < 1.2; bit_period += 0.01) {
+    var decoded = null;
+    // for (var bit_period = 8; bit_period < 10; bit_period += 1/128.0) {
     //     decoded = decoded || try_decode(bit_period);
     // }
 
-    // console.log(history.get(0), decoded);
-}
+    console.log(history.get(0), decoded);
+});
 
 function decode_em(bits) {
     // Test signal, repeating 32-bit manchester code
@@ -100,16 +81,7 @@ function try_decode(bit_period) {
         const sample2 = history.get(Math.round(bit_period * (i * 2 + 1)));
         bits[i] = 0|(sample2 > sample1);
     }
-    // console.log(bits.join(''));
-    return bits[0];
-    // return decode_em(bits);
-}
 
-dev.on('data', (data) => {
-    if (data.length == 9) {
-        do_sample(data.readUInt16LE(1) >> 4);
-        do_sample(data.readUInt16LE(3) >> 4);
-        do_sample(data.readUInt16LE(5) >> 4);
-        do_sample(data.readUInt16LE(7) >> 4);
-    }
-});
+    // console.log(bits.join(''));
+    return decode_em(bits);
+}
